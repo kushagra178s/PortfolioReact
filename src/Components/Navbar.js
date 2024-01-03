e608b72d282aaed05f243fddad6492b32799ce0f
kushagra <kushagra17800@gmail.com>
2024-01-04 04:28:45 +0530

    portfolio

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react" ;
 import {ThemeContext} from "./Main";
 function Navbar() { 
   const {theme, setTheme} = useContext(ThemeContext);
