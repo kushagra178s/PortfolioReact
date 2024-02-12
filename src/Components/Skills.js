bae6a0b93dc6af0e2b8536a8b6e1564a450330c3
kushagra <kushagra17800@gmail.com>
2024-02-13 00:38:54 +0530

    a

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -27,69 +27,93 @@ function Skills() {
       </p>
       <div className="technical-skills">
         <div className="skill-container">
-          <strong>Languages:</strong>
-          <p className="skills-showcase">
+          <table>
+            <tr>
+            <td>
+              <strong>Languages:</strong>
+            </td>
+            <td>
+              <p className="skills-showcase">
+                <div className="skill-card">
+                  <img
+                    src="https://cdn.iconscout.com/icon/free/png-256/free-python-20-1175115.png"
+                    alt=""
+                  />
+                  Python
+                </div>
+                <div className="skill-card">
+                  {" "}
+                  <img src={require("../icons/c++.png")} alt="" /> C++
+                </div>
+                <div className="skill-card">
+                  {" "}
+                  <img src={require("../icons/java.png")} alt="" /> Java
+                </div>
+                <div className="skill-card">
+                  {" "}
+                  <img src={require("../icons/c.png")} alt="" /> C
+                </div>
+                <div className="skill-card">
+                  {" "}
+                  <img src={require("../icons/javascript.png")} alt="" />{" "}
+                  JavaScript
+                </div>
+                <div className="skill-card">
+                  {" "}
+                  <img src={require("../icons/mysql.png")} alt="" /> SQL
+                </div>
+              </p>
+            </td>
+            </tr>
+            <br />
+            <tr>
+            <td>
+              <strong>Technologies:</strong>
+            </td>
+            <td>
+            <p className="skills-showcase">
             <div className="skill-card">
-              <img
-                src="https://cdn.iconscout.com/icon/free/png-256/free-python-20-1175115.png"
-                alt=""
-              />
-              Python
+              {" "}
+              <img src={require("../icons/github.png")} alt="" /> GitHub
             </div>
-            <div className="skill-card"> <img
-                src={require("../icons/c++.png")}
-                alt=""
-              /> C++</div>
-            <div className="skill-card"> <img
-                src={require("../icons/java.png")}
-                alt=""
-              /> Java</div>
-            <div className="skill-card"> <img
-                src={require("../icons/c.png")}
-                alt=""
-              /> C</div>
-            <div className="skill-card"> <img
-                src={require("../icons/javascript.png")}
-                alt=""
-              /> JavaScript</div>
-            <div className="skill-card"> <img
-                src={require("../icons/mysql.png")}
-                alt=""
-              /> SQL</div>
+            <div className="skill-card">
+              {" "}
+              <img src={require("../icons/git.png")} alt="" /> Git
+            </div>
+            <div className="skill-card">
+              {" "}
+              <img src={require("../icons/mongodb.png")} alt="" /> MongoDB
+            </div>
+            <div className="skill-card">
+              {" "}
+              <img src={require("../icons/express.png")} alt="" /> Express
+            </div>
+            <div className="skill-card">
+              {" "}
+              <img src={require("../icons/reactjs.png")} alt="" /> ReactJS
+            </div>
+            <div className="skill-card">
+              {" "}
+              <img src={require("../icons/nodejs.png")} alt="" /> NodeJS
+            </div>
+            <div className="skill-card">
+              {" "}
+              <img src={require("../icons/springbootpng.png")} alt="" />{" "}
+              SpringBoot
+            </div>
           </p>
+
+            </td>
+            </tr>
+            
+          </table>
         </div>
         <div className="skill-container">
-          <strong>Technologies: </strong>
-          <p className="skills-showcase">
-            <div className="skill-card"> <img
-                src={require("../icons/github.png")}
-                alt=""
-              /> GitHub</div>
-            <div className="skill-card"> <img
-                src={require("../icons/git.png")}
-                alt=""
-              /> Git</div>
-            <div className="skill-card"> <img
-                src={require("../icons/mongodb.png")}
-                alt=""
-              /> MongoDB</div>
-            <div className="skill-card"> <img
-                src={require("../icons/express.png")}
-                alt=""
-              /> Express</div>
-            <div className="skill-card"> <img
-                src={require("../icons/reactjs.png")}
-                alt=""
-              /> ReactJS</div>
-            <div className="skill-card"> <img
-                src={require("../icons/nodejs.png")}
-                alt=""
-              /> NodeJS</div>
-            <div className="skill-card"> <img
-                src={require("../icons/springbootpng.png")}
-                alt=""
-              /> SpringBoot</div>
-          </p>
+          <table>
+            
+          </table>
+         
+          
         </div>
       </div>
       <br />
